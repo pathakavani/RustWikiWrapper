eccd48de5b77cce281f3b69a19c9b8085ee13125
Avani Pathak <pathakavani10@gmail.com>
2024-09-30 10:01:48 -0400

    add separate files for models action response structure

---
+++ RustWikiWrapper/src/models/action/mod.rs
@@ -0,0 +1,9 @@
+// src/models/action/mod.rs
+
+pub mod login_response;
+pub mod logout_response;
+pub mod create_account;
+pub mod email_status;
+pub mod page_content;
+pub mod upload_response;
+pub mod user_info;
