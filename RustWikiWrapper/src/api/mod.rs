a05bf692dbedface6e183e3f0974ac1010a1428c
Avani Pathak <pathakavani10@gmail.com>
2024-09-30 09:59:05 -0400

    update mod.rs in api root

--- RustWikiWrapper/src/api/mod.rs
+++ RustWikiWrapper/src/api/mod.rs
@@ -1,27 +1,23 @@
 // src/api/mod.rs
+
 use reqwest::Client;
-use std::error::Error;
 
-use crate::models::SummaryResponse;
+pub mod rest;
+pub mod action;
 
+// Common struct that can be used by all the API's
 pub struct MediaWikiClient {
     client: Client,
     base_url: String,
 }
-``
+
 impl MediaWikiClient {
-    pub fn new(base_url: &str) -> Self {
-        MediaWikiClient {
-            client: Client::new(),
-            base_url: base_url.to_string(),
-        }
+pub fn new(base_url: &str) -> Self {
+    MediaWikiClient {
+        client: Client::new(),
+        base_url: base_url.to_string(),
     }
+}
 
-    pub async fn get_page_summary(&self, title: &str) -> Result<SummaryResponse, Box<dyn Error>> {
-        let url = format!("{}/page/summary/{}", self.base_url, title);
-        let response = self.client.get(&url).send().await?.json::<SummaryResponse>().await?;
-        println!("Parsed Summary Response: {:?}", response);
-
-        Ok(response)
-    }
+// Common utility functions can be added here
 }
