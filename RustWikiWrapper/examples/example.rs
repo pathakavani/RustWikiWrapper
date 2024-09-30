18ab75d54a96269294c693fcbd3656dc9beaa096
Avani Pathak <pathakavani10@gmail.com>
2024-09-30 10:03:09 -0400

    correctly use the crate to run a sample example

--- RustWikiWrapper/examples/example.rs
+++ RustWikiWrapper/examples/example.rs
@@ -1,8 +1,21 @@
+use rust_wiki_wrapper::api::rest::get_page_summary;
 use rust_wiki_wrapper::api::MediaWikiClient;
+use serde_json;
 
 #[tokio::main]
 async fn main() {
     let client = MediaWikiClient::new("https://en.wikipedia.org/api/rest_v1");
-    let summary = client.get_page_summary("Rust_(programming_language)").await.unwrap();
-    println!("Page Summary: {:?}", summary);
+
+    match client.get_page_summary("Rust (programming language)").await {
+        Ok(summary) => {
+            // Pretty print the SummaryResponse as JSON
+            match serde_json::to_string_pretty(&summary) {
+                Ok(pretty_json) => println!("{}", pretty_json),
+                Err(e) => println!("Failed to convert to JSON: {}", e),
+            }
+        }
+        Err(e) => {
+            println!("Failed to fetch the page summary: {}", e);
+        }
+    }
 }
