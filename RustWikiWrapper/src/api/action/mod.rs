b7ea94ecf743e7feceeb1e54b9787e568488cf44
Avani Pathak <pathakavani10@gmail.com>
2024-09-30 10:00:06 -0400

    add separate rs files for action apis

---
+++ RustWikiWrapper/src/api/action/mod.rs
@@ -0,0 +1,10 @@
+// src/api/action/mod.rs
+
+pub mod create_account;
+pub mod create_edit_page;
+pub mod get_current_user;
+pub mod get_page_content;
+pub mod login;
+pub mod logout;
+pub mod send_email;
+pub mod upload_file;
