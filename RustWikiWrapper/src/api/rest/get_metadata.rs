6421a9f5cd623fc451e7824cb4309c53ecc0941c
Avani Pathak <pathakavani10@gmail.com>
2024-09-30 21:59:08 -0400

    supress unused variables warnings

--- RustWikiWrapper/src/api/rest/get_metadata.rs
+++ RustWikiWrapper/src/api/rest/get_metadata.rs
@@ -3,7 +3,7 @@ use crate::models::rest::metadata::Metadata;
 use std::error::Error;
 
 impl MediaWikiClient {
-    pub async fn get_metadata(&self, title: &str) -> Result<Metadata, Box<dyn Error>> {
+    pub async fn get_metadata(&self, _title: &str) -> Result<Metadata, Box<dyn Error>> {
         unimplemented!()
     }
 }
--- RustWikiWrapper/src/api/rest/get_page.rs
+++ RustWikiWrapper/src/api/rest/get_page.rs
@@ -3,7 +3,7 @@ use crate::models::rest::pages::Page;
 use std::error::Error;
 
 impl MediaWikiClient {
-    pub async fn get_page_info(&self, title: &str) -> Result<Page, Box<dyn Error>> {
+    pub async fn get_page_info(&self, _title: &str) -> Result<Page, Box<dyn Error>> {
         unimplemented!()
     }
 }
