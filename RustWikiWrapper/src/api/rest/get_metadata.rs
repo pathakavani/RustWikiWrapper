148e3915ae9838dc153baa22690024ae7dd10fff
Avani Pathak <pathakavani10@gmail.com>
2024-09-30 21:51:10 -0400

    add get_metadata placeholder code

---
+++ RustWikiWrapper/src/api/rest/get_metadata.rs
@@ -0,0 +1,9 @@
+use crate::api::MediaWikiClient;
+use crate::models::rest::metadata::Metadata;
+use std::error::Error;
+
+impl MediaWikiClient {
+    pub async fn get_metadata(&self, title: &str) -> Result<Metadata, Box<dyn Error>> {
+        unimplemented!()
+    }
+}
